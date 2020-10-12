f576b8972e20cf3d2b53738fa3793d231f705c2d
nahjh2 <nahjh2@google.com>
2020-10-12 10:06:04 +0900

    opacity

--- lib/MyAnimatedOpacity.dart
+++ lib/MyAnimatedOpacity.dart
@@ -6,16 +6,35 @@ class MyAnimatedOpacity extends StatefulWidget {
 }
 
 class _MyAnimatedOpacityState extends State<MyAnimatedOpacity> {
+  bool _visible = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("AnimatedOpacity"),
       ),
-      body: Container(),
+
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: Duration(milliseconds: 500),
+
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.green,
+          ),
+        ),
+      ),
+
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.play_arrow),
-        onPressed: null,
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
       ),
     );
   }
